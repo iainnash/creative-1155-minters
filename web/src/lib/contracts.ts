3066a0e6edddb2544941199ae101d49effd73ff4
iain nash <me@iain.in>
2023-06-26 18:26:09 -0400

    update contracts addresses

--- web/src/lib/contracts.ts
+++ web/src/lib/contracts.ts
@@ -3,6 +3,6 @@ import { Address } from "viem";
 
 export const CONTRACT_BY_NETWORK = {
   [zoraTestnet.id]: "0x45800a5853091ba3D992De3a114eDBc8F6DaD039" as Address,
-  [zora.id]: "0x6E269B9E7eaBe364c36744D2988687e8F47C2f9a" as Address,
+  [zora.id]: "0xa306BF2973dAd706E5685f051A9DfD7Df1CdDcBD" as Address,
   [goerli.id]: "0x2572259361ee3100e388b75d91b807bda2c8b118" as Address,
 } as const;
