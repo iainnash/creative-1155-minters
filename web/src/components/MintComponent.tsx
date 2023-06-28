432fb7221458b2561654645a4f250ae69a967def
iain nash <me@iain.in>
2023-06-28 03:14:15 -0400

    minting componet fix

--- web/src/components/MintComponent.tsx
+++ web/src/components/MintComponent.tsx
@@ -64,7 +64,7 @@ export const MintComponent = ({
       abi: zoraCreator1155FactoryImplABI,
       functionName: "createContract",
       args: [
-        "",
+        "ipfs://bafkreihu6ihpfyozydukhszmwtcomdaj6qgmpogde34w7cfo5v4m66oilm",
         "P5JS Mints",
         {
           royaltyMintSchedule: 0,
